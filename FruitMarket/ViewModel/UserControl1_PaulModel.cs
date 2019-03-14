420bf63836d96078dd5c34fa17df156e39f67168
pmuller <pmuller@CE20550>
2019-03-14 09:23:23 +0100

    product list und alphabetische liste optimiert

--- FruitMarket/ViewModel/UserControl1_PaulModel.cs
+++ FruitMarket/ViewModel/UserControl1_PaulModel.cs
@@ -22,9 +22,16 @@ namespace FruitMarket.ViewModel
 
         public UserControl1_PaulModel()
         {
-            m_Fruits.Add(new Fruit("Sort 1", 1, "Cat. 1", new Supplier("Lidl"), DateTime.Now, TimeSpan.FromHours(12), "Spain", 2.50, 3.99));
-            m_Fruits.Add(new Fruit("Sort 2", 1, "Cat. 2", new Supplier("Aldi"), DateTime.Now, TimeSpan.FromHours(24), "Italy", 3.50, 5.99));
-            m_Fruits.Add(new Fruit("Sort 3", 1, "Cat. 3", new Supplier("Apple"), DateTime.Now, TimeSpan.FromHours(48), "Germany", 4.50, 7.99));
+            m_Fruits.Add(new Fruit("Hokkaidokürbis", 1337, "Kat. 2", new Supplier("Biomüll-Wiederaufbereitung KG"), DateTime.Now.AddDays(20), TimeSpan.FromHours(24), "Italien, -", 3.50, 5.99));
+            m_Fruits.Add(new Fruit("Apfel", 1000, "Kat. 1", new Supplier("Grossman KG"), DateTime.Now.AddDays(2), TimeSpan.FromHours(14), "Deutschland, -", 2.50, 3.99));
+            m_Fruits.Add(new Fruit("Drachenfrucht", 30, "Kat. 3", new Supplier("Grossman KG"), DateTime.Now.AddDays(2), TimeSpan.FromHours(96), "China, - ", 4.50, 7.99));
+            m_Fruits.Add(new Fruit("Hokkaidokürbis", 20, "Kat. 3", new Supplier("Grossman KG"), DateTime.Now.AddDays(30), TimeSpan.FromHours(96), "Deutschland, -", 4.50, 7.99));
+            m_Fruits.Add(new Fruit("Wassermelone", 0, "Kat. 3", new Supplier("Gassel AG"), DateTime.Now, TimeSpan.FromHours(48), "Vietnam, Phu Tho", 4.50, 12.99));
+            m_Fruits.Add(new Fruit("Wassermelone", 0, "Kat. 3", new Supplier("Grossman KG"), DateTime.Now, TimeSpan.FromHours(48), "Vietnam, Phu Tho", 4.50, 7.99));
+            m_Fruits.Add(new Fruit("Drachenfrucht", 0, "Kat. 3", new Supplier("Kellerman GmbH"), DateTime.Now.AddDays(20), TimeSpan.FromHours(48), "USA, Arizona", 4.50, 7.99));
+            m_Fruits.Add(new Fruit("Hokkaidokürbis", 42, "Kat. 3", new Supplier("Kellerman GmbH"), DateTime.Now.AddDays(40), TimeSpan.FromHours(48), "Deutschland, -", 4.50, 7.99));
+            m_Fruits.Add(new Fruit("Hokkaidokürbis", 42, "Kat. 3", new Supplier("Zettelman AG"), DateTime.Now.AddDays(50), TimeSpan.FromHours(48), "Deutschland, -", 4.50, 7.99));
+
         }
     }
 }
