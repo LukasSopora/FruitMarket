6233cce465dd621ac07f0a5caba5b7697f661153
pmuller <pmuller@CE20550>
2019-03-17 12:14:56 +0100

    filterview added

--- FruitMarket/View/FilterView.xaml.cs
+++ FruitMarket/View/FilterView.xaml.cs
@@ -24,5 +24,10 @@ namespace FruitMarket.View
         {
             InitializeComponent();
         }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
