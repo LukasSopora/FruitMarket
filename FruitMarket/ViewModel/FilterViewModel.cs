0c18469754ec4088cb7d50876ce2d3d3910b7ef2
Lukas Sopora <lukas@sopora.de>
2019-03-24 22:23:46 +0100

    - implement FilterViewModel

--- FruitMarket/ViewModel/FilterViewModel.cs
+++ FruitMarket/ViewModel/FilterViewModel.cs
@@ -1,12 +1,95 @@
-﻿using System;
+﻿using FruitMarket.Model;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FruitMarket.ViewModel
 {
-    class FilterViewModel
+    public class FilterViewModel : BindableBase
     {
+        private ObservableCollection<Filter> m_Filter =
+            new ObservableCollection<Filter>();
+        private ObservableCollection<Product> m_Products =
+            new ObservableCollection<Product>();
+        private Filter m_CurrentFilter = null;
+
+        public ObservableCollection<Product> Products
+        {
+            get { return m_Products; }
+            set { SetProperty(ref m_Products, value); }
+        }
+
+        public Filter CurrentFilter
+        {
+            get { return m_CurrentFilter; }
+            set { SetProperty(ref m_CurrentFilter, value); }
+        }
+
+        public ObservableCollection<Filter> Filter
+        {
+            get { return m_Filter; }
+            set { SetProperty(ref m_Filter, value); }
+        }
+
+        #region Commands
+        public DelegateCommand AddFilterCommand { get; private set; }
+        public DelegateCommand DeleteFilterCommand { get; private set; }
+        public DelegateCommand DeleteAllCommand { get; private set; }
+
+        private void InitializeCommands()
+        {
+            AddFilterCommand = new DelegateCommand(OnAddFilter);
+            RaisePropertyChanged(nameof(AddFilterCommand));
+            DeleteFilterCommand = new DelegateCommand(OnDeleteFilter);
+            RaisePropertyChanged(nameof(DeleteAllCommand));
+            DeleteAllCommand = new DelegateCommand(OnDeleteAll);
+            RaisePropertyChanged(nameof(DeleteAllCommand));
+        }
+
+        private void OnDeleteAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnDeleteFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnAddFilter()
+        {
+            if(m_CurrentFilter == null)
+            {
+                return;
+            }
+            if(m_CurrentFilter.Property == null)
+            {
+                System.Windows.MessageBox.Show("Kein Filterkriterium ausgewählt.");
+            }
+            if(m_CurrentFilter.FilterText == null)
+            {
+                m_CurrentFilter.FilterText = "";
+            }
+            if(!m_Filter.Contains(m_CurrentFilter))
+            {
+                m_Filter.Add(m_CurrentFilter);
+                RaisePropertyChanged(nameof(Filter));
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Filter ist bereits angewendet.");
+            }
+        }
+        #endregion
+
+        public FilterViewModel()
+        {
+            InitializeCommands();
+        }
     }
 }
