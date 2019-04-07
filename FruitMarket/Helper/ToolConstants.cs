e8b45c4026e308d22ef6419a96d9244d5d2c60d5
Lukas Sopora <lukas@sopora.de>
2019-04-07 03:02:28 +0200

    - add DeliveryNote table - adapt deliverytable

--- FruitMarket/Helper/ToolConstants.cs
+++ FruitMarket/Helper/ToolConstants.cs
@@ -203,23 +203,24 @@ namespace FruitMarket.Helper
 
         #region Delivery
         public const string DB_DELIVERY_TABLE = "Delivery";
-        public const string DB_DELIVERY_ID = "Id";
-        public const string DB_DELIVERY_PRODUCT_ID = "ProductId";
         public const string DB_DELIVERY_COSTUMER_ID = "CostumerId";
-        public const string DB_DELIVERY_DATA = "Data";
+        public const string DB_DELIVERY_PRODUCT_ID = "ProductId";
+        public const string DB_DELIVERY_DELIVERYNOTE_ID = "DeliveryNoteId";
+        public const string DB_DELIVERY_AMOUNT = "Amount";
 
         public static string DB_DELIVERY = string.Format(
             "CREATE TABLE IF NOT EXISTS {0} (" +
-            "{1} INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            "{2} TEXT, " +
-            "{3} TEXT, " +
-            "{4} BLOB);", new string[]
+            "{1} INTEGER references {2}({3}), " +
+            "{4} INTEGER references {5}({6}), " +
+            "{7} INTEGER references {8}({9}), " +
+            "{10} INTEGER, " +
+            "PRIMARY KEY ({1}, {4}, {7}));", new string[]
             {
                 DB_DELIVERY_TABLE,
-                DB_DELIVERY_ID,
-                DB_DELIVERY_PRODUCT_ID,
-                DB_DELIVERY_COSTUMER_ID,
-                DB_DELIVERY_DATA
+                DB_DELIVERY_COSTUMER_ID, DB_COSTUMER_TABLE, DB_COSTUMER_ID,
+                DB_DELIVERY_PRODUCT_ID, DB_PRODUCT_TABLE, DB_PRODUCT_ID,
+                DB_DELIVERY_DELIVERYNOTE_ID, DB_DELIVERYNOTE_TABLE, DB_DELIVERYNOTE_ID,
+                DB_DELIVERY_AMOUNT
             });
         #endregion
 
